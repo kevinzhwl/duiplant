0359e683f3853ca0a89d56c0ab1c5db0a0e3e216
setoutsoft <setoutsoft@17d578f1-4aff-4a0d-8bc7-daf1faacbcb7>
2014-02-18 15:26:00 +0000

    git-svn-id: http://code.taobao.org/svn/duiengine/trunk@126 17d578f1-4aff-4a0d-8bc7-daf1faacbcb7

---
+++ controls.extend/DuiColorPicker.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "ColourPopup.h"
+
+namespace DuiEngine
+{
+
+class CDuiColorPicker :
+	public CDuiButton ,
+	public IColorPicker
+{
+	DUIOBJ_DECLARE_CLASS_NAME(CDuiColorPicker, "colorpicker")
+
+public:
+	CDuiColorPicker(void);
+	~CDuiColorPicker(void);
+
+protected://IColorPicker
+	virtual void OnColorChanged(COLORREF cr);
+	virtual void OnColorEnd(BOOL bCancel,COLORREF cr);
+protected:
+	void OnPaint(CDCHandle dc);
+	void OnLButtonUp(UINT nFlags,CPoint pt);
+
+	DUIWIN_BEGIN_MSG_MAP()
+		MSG_WM_PAINT(OnPaint)
+		MSG_WM_LBUTTONUP(OnLButtonUp)
+	DUIWIN_END_MSG_MAP()
+
+	DUIWIN_DECLARE_ATTRIBUTES_BEGIN()
+		DUIWIN_COLOR_ATTRIBUTE("color",m_crCur=m_crDef,TRUE)
+	DUIWIN_DECLARE_ATTRIBUTES_END()
+	
+	COLORREF	m_crDef;
+	COLORREF	m_crCur;
+};
+
+}
