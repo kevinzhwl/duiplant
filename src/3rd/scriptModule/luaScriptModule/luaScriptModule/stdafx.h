7c09b0db3988de8c98ce01fbe4b55eb42ad72caa
kevinzhwl <kevinzhwl@gmail.com>
2014-04-28 10:06:22 +0800

    更新库名字

--- src/3rd/scriptModule/luaScriptModule/luaScriptModule/stdafx.h
+++ src/3rd/scriptModule/luaScriptModule/luaScriptModule/stdafx.h
@@ -16,11 +16,11 @@
 #endif
 
 	#if defined(USING_ATL)
-	# pragma comment(lib, "duiengine-win32-debug_atl.lib")
+	# pragma comment(lib, "duiengine-win32-debug-atl.lib")
 	#elif defined(DLL_DUI)
-	# pragma comment(lib, "duiengine-win32-debug_shared.lib")
+	# pragma comment(lib, "duiengine-win32-debug-shared.lib")
 	#else
-	# pragma comment(lib, "duiengine-win32-debug_static.lib")
+	# pragma comment(lib, "duiengine-win32-debug-static.lib")
 	#endif
 #else
 
@@ -29,11 +29,11 @@
 #endif
 
 	#if defined(USING_ATL)
-	# pragma comment(lib, "duiengine-win32-release_atl.lib")
+	# pragma comment(lib, "duiengine-win32-release-atl.lib")
 	#elif defined(DLL_DUI)
-	# pragma comment(lib, "duiengine-win32-release_shared.lib")
+	# pragma comment(lib, "duiengine-win32-release-shared.lib")
 	#else
-	# pragma comment(lib, "duiengine-win32-release_static.lib")
+	# pragma comment(lib, "duiengine-win32-release-static.lib")
 	#endif
 
 #endif
