4c442d25ab400fa0ed6b20c5e48012a4aafb8d17
kevinzhwl <kevinzhwl@gmail.com>
2014-05-11 18:24:19 +0800

    初步完成顶层的cmake

--- src/cmake/duix_build_config.cmake
+++ src/cmake/duix_build_config.cmake
@@ -1,2 +1,14 @@
 
+
+set(CMAKE_BUILD_TYPE Debug)
+
 set(BUILD_examples ON)
+set(BUILD_duixengine ON)
+
+
+set(BUILD_duixengine_shared "Build Shared Libraries" ON)
+set(BUILD_duixengine_static "Build Static Libraries" OFF)
+
+set(BUILD_TARGET_WIN64_PLATFORM "Build Target for X64 Platform" OFF)
+set(DUIX_BUILD_INSTALLATION "Build DuixEngine Installation" OFF)
+#OPTION (DUIX_BUILD_RELEASE "Build Release Version" OFF)
