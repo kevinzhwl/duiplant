97bc13e466480f2ab7f6609cfc149184eca8fbe6
kevinzhwl <kevinzhwl@gmail.com>
2014-05-13 23:36:50 +0800

    增加构建winres的命令

--- src/cmake/duix_build_config.cmake
+++ src/cmake/duix_build_config.cmake
@@ -3,12 +3,11 @@
 set(CMAKE_BUILD_TYPE Debug)
 
 set(BUILD_examples ON)
+
 set(BUILD_duixengine ON)
+set(BUILD_duixengine_shared ON)
+set(BUILD_duixengine_static OFF)
 
-
-set(BUILD_duixengine_shared "Build Shared Libraries" ON)
-set(BUILD_duixengine_static "Build Static Libraries" OFF)
-
-set(BUILD_TARGET_WIN64_PLATFORM "Build Target for X64 Platform" OFF)
-set(DUIX_BUILD_INSTALLATION "Build DuixEngine Installation" OFF)
-#OPTION (DUIX_BUILD_RELEASE "Build Release Version" OFF)
+set(BUILD_TARGET_WIN64_PLATFORM OFF)
+set(DUIX_BUILD_INSTALLATION OFF)
+# (DUIX_BUILD_RELEASE OFF)
