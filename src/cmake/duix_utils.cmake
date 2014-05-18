bc9f1d9270c3ced8713249684ad3b635e03cfe76
kevinzhwl <kevinzhwl@gmail.com>
2014-05-18 20:51:07 +0800

    修复失效的xml创建命令

--- src/cmake/duix_utils.cmake
+++ src/cmake/duix_utils.cmake
@@ -2,10 +2,11 @@
 
 macro(SETDUIX_TOOLS_DIR _basedir)
   if (NOT DEFINED RESBUILDER_NAME)
-    set(RESBUILDER_NAME "residbuilder2.exe")
+    set(RESBUILDER_NAME "residbuilder21.exe")
   endif (NOT DEFINED RESBUILDER_NAME)
+
   if (NOT DEFINED ZIPBUILDER_NAME)
-    set(RESBUILDER_NAME "7z.exe")
+    set(ZIPBUILDER_NAME "7z.exe")
   endif (NOT DEFINED ZIPBUILDER_NAME)
 
     set(TOOLS_ROOT ${_basedir})
@@ -17,9 +18,10 @@ endmacro(SETDUIX_TOOLS_DIR)
 macro(GETDUIX_BUILD_CMD _rescmd _xmlfile _rcfile)
 
 if(NOT DEFINED DUIX_RESBUILDER)
-    message(WARNING "Not Found Resbuilder")
+    message(STATUS "Not Found Resbuilder")
 else(NOT DEFINED DUIX_RESBUILDER)
-    set(_rescmd ${RESBULDER} -i ${xmlfile} -y -p skin -r ${_rcfile})
+    set(_rescmd)
+    set(_rescmd "${DUIX_RESBUILDER} -i ${_xmlfile} -y -p skin -r ${_rcfile}")
 endif(NOT DEFINED DUIX_RESBUILDER)
 
 endmacro(GETDUIX_BUILD_CMD)
